256e7b90fb28c2121ef8d96d1fe7e6bca5d74b2b
Charis <tam.charis@gmail.com>
2025-08-06 09:48:41 -0700

    hardcoded frontend of login screen and imported font

--- client/src/LoginScreen.tsx
+++ client/src/LoginScreen.tsx
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import './LoginScreen.css';
+
 
 const LoginScreen: React.FC = () => {
     return (
-        <div>
-            <p>
-                this will be the login screen someday!
-            </p>
-            <p>
-                <Link to="/input">go to text input screen (temporary!)</Link>
-            </p>
-            <p>
-                <Link to="/flashcard">go to flashcard screen (temporary!)</Link>
-            </p>
+        <div className="parent-div">
+            <div className="login-div">
+                <h1 className="title">recallify</h1>
+                <p className="register-option">don't have an account? register here.</p>
+                <div className="textbox">username</div>
+                <div className="textbox">password</div>
+                <p className="forgot-password">forgot password?</p>
+                <button>sign in</button>
+            </div>
+            <div className="temp-links">
+                <p><Link to="/input">go to text input screen (temporary!)</Link></p>
+                <p><Link to="/flashcard">go to flashcard screen (temporary!)</Link></p>
+            </div>
         </div>
     );
 }
