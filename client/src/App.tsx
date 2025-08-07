ba33f296e45d88bf09d48f00c7e414276a5c94e9
Charis <tam.charis@gmail.com>
2025-08-07 15:42:18 -0700

    set up event handling for text input

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import LoginScreen from './LoginScreen';
@@ -7,10 +7,18 @@ import FlashcardScreen from './FlashcardScreen';
 
 
 const App: React.FC = () => {
+  const [flashcardData, setFlashcardData] = useState(null);
+
+  const getFlashcards = async () => {
+    console.log("getFlashcards call successful");
+    // api call with userInput
+    //setFlashcardData(data);
+  }
+
   return (
     <Routes>
       <Route path = "/" element={<LoginScreen/>} />
-      <Route path = "/input" element={<InputScreen/>} />
+      <Route path = "/input" element={<InputScreen onSubmit={getFlashcards}/>} />
       <Route path = "/flashcard" element={<FlashcardScreen/>} />
     </Routes>
   );
--- client/src/InputScreen.tsx
+++ client/src/InputScreen.tsx
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './InputScreen.css';
 import { Link } from 'react-router-dom';
 import { Box, Stack, Textarea, Text, Button } from "@chakra-ui/react";
 
-const InputScreen: React.FC = () => {
+export interface InputScreenProps {
+    onSubmit: (userInput: string) => void; // function that accepts userInput and does not return anything
+    // isLoading: boolean;
+}
+
+const InputScreen: React.FC<InputScreenProps> = ({ onSubmit }) => {
+    const [inputText, setInputText] = useState('');
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setInputText(event.target.value);
+        console.log(inputText);
+    };
+
+    // event handler called by button onClick event
+    const handleSubmit = () => {
+        console.log("Stored text: " + inputText);
+        onSubmit(inputText); // passes text to prop function onSubmit
+    }
+
     return (
         <div className="parent-div">
             <Stack direction="column" align="center">
                 <Box w="900px" padding="15px" rounded="xl" bg="#e6f1f5">
                     <Text color="#4B5358" textStyle="xl">what would you like to study today? </Text>
                 </Box>
-                <Textarea h="320px" placeholder="enter text here (1000 words max)."/>
-                <Button size="lg" rounded="xl" bg="#e6f1f5" w="260px" color="#4B5358">
+                <Textarea 
+                    h="320px" 
+                    value={inputText}
+                    onChange={handleInputChange}
+                    placeholder="enter text here (1000 words max)."
+                />
+                <Button 
+                    size="lg" rounded="xl" bg="#e6f1f5" w="260px" color="#4B5358"
+                    onClick={handleSubmit}
+                >
                     generate flashcards
                 </Button>
             </Stack>
