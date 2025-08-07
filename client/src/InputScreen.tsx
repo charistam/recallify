c5ef26d66d509913902b7150a4ae8a578807cb68
Charis <tam.charis@gmail.com>
2025-08-06 23:15:47 -0700

    initial layout and design of text input page

--- client/src/InputScreen.tsx
+++ client/src/InputScreen.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import './InputScreen.css';
 import { Link } from 'react-router-dom';
-import { Button } from "@chakra-ui/react";
+import { Box, Stack, Textarea, Text, Button } from "@chakra-ui/react";
 
 const InputScreen: React.FC = () => {
     return (
-        <div>
-            INPUT UR STUDY TEXT HERE!!!!
-            <Button>Click</Button>
+        <div className="parent-div">
+            <Stack direction="column" align="center">
+                <Box w="900px" padding="15px" rounded="xl" bg="#e6f1f5">
+                    <Text textStyle="xl">what would you like to study today? </Text>
+                </Box>
+                <Textarea h="320px" placeholder="enter text here (1000 words max)."/>
+                <Button size="lg" rounded="xl" bg="#e6f1f5" w="260px" color="#4B5358">
+                    generate flashcards
+                </Button>
+            </Stack>
+
+            <div className="temp-links">
+                <p><Link to="/">go to login screen (temporary!)</Link></p>
+                <p><Link to="/flashcard">go to flashcard screen (temporary!)</Link></p>
+            </div>
         </div>
     );
 }
